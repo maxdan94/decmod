4456aa6f3b850ee91cb7242bb08ead498d3a8b65
Vladimir Antonov <antonovvk@yandex.ru>
2017-12-06 19:06:04 +0300

    C++ compatibility, printing function header, remove trailing spaces

--- dm_english.h
+++ dm_english.h
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 MODULAR DECOMPOSITION OF UNDIRECTED GRAPHS
 by Fabien de Montgolfier
 
-The program dm.c offer a single function, 
+The program dm.c offer a single function,
 decomposition_modulaire().
 
 The input is a graph, its output is its modular decomposition tree.
@@ -40,14 +40,17 @@ Definition of graphs (for the input)
 The graph has n vertices. Each vertex is numbered from 0 to n-1.
 A graph is a structure (not an object-oriented program !).
 If you declare "graphe Gr", Gr.n is the numer of vertices of Gr.
-Gr.G is an array of size n. Gr.G[i] points the first element 
+Gr.G is an array of size n. Gr.G[i] points the first element
 of adjaceny list of vertex i (NULL if i is isolated)
 An adjency list is an usual linked list (vertex;pointer to next).
 Adjacency lists may be unsorted.
 
-WARNING : the input graph *MUST* be symmetric : if j belongs to adjacency list of i then i belongs to adjacency list of j. Graphs that do not respect this produce unpredictible and false results. 
+WARNING : the input graph *MUST* be symmetric : if j belongs to adjacency list of i then i belongs to adjacency list of j. Graphs that do not respect this produce unpredictible and false results.
 **********************************/
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* structure for creating an adjacency list */
 typedef struct Adj {
@@ -71,10 +74,10 @@ As the tree is an inclusion tree, the vertex-set corresponding to an internal no
 
 /* define the type of nodes. UNKN,MODULE,ARTEFACT are for internal use*/
 
-#define FEUILLE 0  // the node is a leaf   
-#define UNKN 1		
-#define MODULE 2	
-#define ARTEFACT 3	
+#define FEUILLE 0  // the node is a leaf
+#define UNKN 1
+#define MODULE 2
+#define ARTEFACT 3
 #define SERIE 4    // series composition
 #define PARALLELE 5  // parallel composition
 #define PREMIER 6  // prime composition
@@ -82,12 +85,12 @@ As the tree is an inclusion tree, the vertex-set corresponding to an internal no
 /* defines a node of the tree */
 
 typedef struct Noeud {
-  int type;	// is FEUILLE, SERIE, PARALLELE or PREMIER		
+  int type;	// is FEUILLE, SERIE, PARALLELE or PREMIER
   struct Noeud *pere;	// adress of parent node, NULL if root
   struct Fils *fpere;	// points the head of the linked list of sons (if type is not FEUILLE, else is NULL)
   int nom;	// if type=FEUILLE, number of the corresponding vertex of the graph
   struct Fils *fils; // points the head of the linked list of sons
-  struct Fils *lastfils;  // internal use (points the last item in the listed list of sons) 
+  struct Fils *lastfils;  // internal use (points the last item in the listed list of sons)
   int id;	// internal use (node unique ID)
   int fv;  // internal use (first vertex in factorizing permutation)
   int lv;  // internal use (last vertex in factorizing permutation)
@@ -106,10 +109,8 @@ typedef struct Fils {
 
 noeud *decomposition_modulaire(graphe G);
 
+void printarbre(noeud * N);
 
-
-
-
-
-
-
+#ifdef __cplusplus
+}
+#endif
